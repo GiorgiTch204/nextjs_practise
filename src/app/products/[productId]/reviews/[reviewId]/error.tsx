01e1947305150f41220716b23cdef1297829c5b9
chey134 <giotchey@gmail.com>
2026-08-04 15:31:58 +0400

    Added revovering from errors part

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-export default function ErrorBoundary({error} :{
+import {useRouter} from "next/navigation";
+import {startTransition} from "react";
+
+export default function ErrorBoundary({error, reset} :{
     error: Error,
+    reset: () => void;
 }){
-    return <div>{error.message}</div>
+    const router = useRouter();
+        const reload = () =>{
+            startTransition(()=>{
+                router.refresh();
+                reset();
+            });
+        };
+
+    return(
+        
+
+        <div>
+            {error.message}
+            <button onClick={reload}>Try again</button>
+        </div>
+    ) 
+}
+
+function refresh() {
+    throw new Error("Function not implemented.");
 }
