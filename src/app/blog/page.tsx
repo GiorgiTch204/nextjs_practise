85e0669b761fcc3fd473e3e1c4657ce84a31dc6f
chey134 <giotchey@gmail.com>
2026-08-04 15:03:00 +0400

    Created loading.tsx file for loading UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,8 @@
-"use client";
-
-export default function Blog(){
+export default async function Blog(){
+    await new Promise(resolve => {
+        setTimeout(() => {
+            resolve("done");
+        }, 1000);
+    });
     return <h1>Blog Page</h1>
 };
